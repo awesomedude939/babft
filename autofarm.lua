c92c757e67effc915b5ce32f4504d2d4636607a6
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-01-27 19:03:36 -0300

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -8,18 +8,24 @@ local plr = game:GetService("Players").LocalPlayer
 local char = plr.Character
 
 local t1 = ts:Create(char.HumanoidRootPart,TweenInfo.new(0.05),{CFrame = CFrame.new(-49.5, 26.3, 999.75)})
-local t2 = ts:Create(char.HumanoidRootPart,TweenInfo.new(getgenv().speed-4),{CFrame = CFrame.new(-49.5, 26.5, 8706.75)})
+local t2 = ts:Create(char.HumanoidRootPart,TweenInfo.new(getgenv().speed-4),{CFrame = CFrame.new(-49.5, 26.3, 8706.75)})
 local t3 = ts:Create(char.HumanoidRootPart,TweenInfo.new(2),{CFrame = CFrame.new(-49.5, -298.5, 9050)})
 local t4 = ts:Create(char.HumanoidRootPart,TweenInfo.new(2),{CFrame = CFrame.new(-57.5, -357.5, 9496.5)})
 
 repeat
 if not getgenv().auto then break end
+local s,e = pcall(function()
 t1:Play()
 t1.Completed:Wait()
+t1:Pause()
 t2:Play()
 t2.Completed:Wait()
+t2:Pause()
 t3:Play()
 t3.Completed:Wait()
-t4.Play()
+t3:Pause()
+t4:Play()
 t4.Completed:Wait()
+t4:Pause()
+end)
 until not getgenv().auto
